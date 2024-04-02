af71f4befa1a55bc983d45b370e3ae63469fc3b9
Nmcleon <nmcleon10@gmail.com>
2024-04-03 01:57:41 +0200

    Modification

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
- entry: './js/dashboard_main.js',
+ entry: path.resolve(__dirname, './js/dashboard_main.js'),
+ performance: {
+    maxAssetSize: 1000000,
+  },
  output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
