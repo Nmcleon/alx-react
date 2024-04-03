7177e8c072e1c15a11ccb88f45f808c039bde845
Nmcleon <nmcleon10@gmail.com>
2024-04-03 11:01:34 +0200

    BS

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,10 +1,31 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+  entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
-  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ],
+ },
+ mode: 'production',
 };
