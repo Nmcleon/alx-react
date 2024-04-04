46bf7fdf88fd709c62956ab4d1624499fde4efac
Nmcleon <nmcleon10@gmail.com>
2024-04-04 10:41:13 +0200

    Test files

--- 0x01-react_intro/task_3/dashboard/src/App.js
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -11,6 +11,11 @@ function App() {
     </header>
       <body className="App-body">
         <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>Email: </label>
+        <input type='email' id='email' />
+        <label htmlFor="password"> Password: </label>
+        <input type="password" id="password" />
+        <button>OK</button>
       </body>
       <footer className="App-footer">
         <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
