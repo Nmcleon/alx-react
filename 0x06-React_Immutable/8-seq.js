4af41834316b68c3db33c23db3e26c3803e65162
Nmcleon <nmcleon10@gmail.com>
2024-04-24 22:01:48 +0200

    Lazy Seq

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,18 @@
+import { Seq } from './node_modules/immutable/dist/immutable';
+
+export default function printBestStudent(object) {
+  const seq = Seq(object);
+  const getStudent = seq.filter((value) => value.score > 70);
+
+  const student = getStudent.toJS();
+
+  const fprmatName = (name) => name.chartAt(0).toUpperCase() + name.slice(1);
+
+  Object.keys(student).map((key) => {
+    student[key].firstName = formatName(student[key].firstName);
+    student[key].lastName = formatName(student[key].lastName);
+    return student[key];
+  });
+
+  console.log(student);
+}
