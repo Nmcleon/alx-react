046cb1423bbf1b2c9bdaf424a42da1153d34508f
Nmcleon <nmcleon10@gmail.com>
2024-05-01 15:05:02 +0200

    Bound the actions

---
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,37 @@
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+} from './uiActionTypes';
+import { bindActionCreators } from 'redux';
+
+export const login = (email, password) => ({
+  type: LOGIN,
+  user: {
+    email: email,
+    password: password,
+  },
+});
+
+export const logout = () => ({
+  type: LOGOUT,
+});
+
+export const displayNotificationDrawer = () => ({
+  type: DISPLAY_NOTIFICATION_DRAWER,
+});
+
+export const hideNotificationDrawer = () => ({
+  type: HIDE_NOTIFICATION_DRAWER,
+});
+
+export const boundUIActionCreators = bindActionCreators(
+  {
+    login: login,
+    logout: logout,
+    displayNotificationDrawer: displayNotificationDrawer,
+    hideNotificationDrawer: hideNotificationDrawer,
+  },
+  dispatch
+);
